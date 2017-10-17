85e18d6c90aea8c1f4e65fe2094a948acc38affa
Lara Aasem <lara-aasem@hotmail.com>
2017-10-17 17:34:20 +0200

    Make calculator

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,51 @@
+def add(op1, op2)
+	return op1 + op2
+end
 
+def subtract(op1, op2)
+	return op1 - op2
+end
+
+def multiply(op1, op2)
+	return op1 * op2
+end
+
+def divide(op1, op2)
+	return op1 / op2
+end
+
+def power(op1, op2)
+	counter = 0
+	expo = 1
+	while counter < op2
+		expo *= op1
+		counter += 1
+	end
+	return expo
+end
+
+def calculator
+	puts 'Please enter operand 1, operand 2, and the operation you want to perform [+, -, *, /] as comma-separated values'
+	input_array = gets.chomp.split(',')
+	op1 = input_array[0].to_i
+	op2 = input_array[1].to_i
+	case input_array[2]
+	when '+'
+		result = add(op1, op2)
+	when '-'
+		result = subtract(op1, op2)
+	when '*'
+		result = multiply(op1, op2)
+	when '/'
+		result divide(op1, op2)
+	when '^'
+		# result = op1 ** op2
+		result = power(op1, op2)
+	else
+		puts "Invalid operation"
+		result = "Invalid"
+	end
+	puts %{Result is: #{result}}
+end
+
+calculator
